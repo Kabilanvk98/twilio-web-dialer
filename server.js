cc20d62d3bfb4101f94518def6f8b13bc56543c4
Kabilanvk98 <57007075+Kabilanvk98@users.noreply.github.com>
2026-01-09 01:53:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
+const express = require("express");
+const twilio = require("twilio");
+
+/* CREATE APP FIRST */
+const app = express();
+
+/* MIDDLEWARE */
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+
+/* ENVIRONMENT VARIABLES */
+const ACCOUNT_SID = process.env.ACCOUNT_SID;
+const API_KEY = process.env.API_KEY;
+const API_SECRET = process.env.API_SECRET;
+const CALLER_ID = process.env.CALLER_ID;
+
+/* SIMPLE START LOG (CONFIRMS DEPLOY) */
+console.log("SERVER STARTED – FINAL VERSION");
+
+/* TOKEN ENDPOINT FOR BROWSER */
 app.get("/token", (req, res) => {
   const AccessToken = twilio.jwt.AccessToken;
   const VoiceGrant = AccessToken.VoiceGrant;
@@ -7,13 +27,35 @@ app.get("/token", (req, res) => {
   });
 
   const token = new AccessToken(
-    process.env.ACCOUNT_SID,
-    process.env.API_KEY,
-    process.env.API_SECRET,
+    ACCOUNT_SID,
+    API_KEY,
+    API_SECRET,
     { identity: "agent1" }
   );
 
   token.addGrant(voiceGrant);
 
   res.json({ token: token.toJwt() });
+});
+
+/* VOICE WEBHOOK – THIS ACTUALLY DIALS */
+app.post("/voice", (req, res) => {
+  console.log("VOICE WEBHOOK HIT", req.body);
+
+  const twiml = new twilio.twiml.VoiceResponse();
+  const to = req.body.To;
+
+  if (to) {
+    twiml.dial({ callerId: CALLER_ID }, to);
+  } else {
+    twiml.say("No destination number provided");
+  }
+
+  res.type("text/xml");
+  res.send(twiml.toString());
+});
+
+/* START SERVER */
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server listening");
 });
