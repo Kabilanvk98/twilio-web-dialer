64c7190c1d91ddbb5040766e26df87fd67f0feaf
Kabilanvk98 <57007075+Kabilanvk98@users.noreply.github.com>
2026-01-09 01:10:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,15 +3,19 @@ const twilio = require("twilio");
 const app = express();
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
 const ACCOUNT_SID = process.env.ACCOUNT_SID;
 const API_KEY = process.env.API_KEY;
 const API_SECRET = process.env.API_SECRET;
-const APP_SID = process.env.APP_SID;
 
 app.get("/token", (req, res) => {
   const AccessToken = twilio.jwt.AccessToken;
   const VoiceGrant = AccessToken.VoiceGrant;
+
+  const voiceGrant = new VoiceGrant({
+    outgoingApplicationSid: "unused"
+  });
 
   const token = new AccessToken(
     ACCOUNT_SID,
@@ -20,14 +24,23 @@ app.get("/token", (req, res) => {
     { identity: "agent1" }
   );
 
-  token.addGrant(
-    new VoiceGrant({
-      outgoingApplicationSid: APP_SID,
-      incomingAllow: true
-    })
-  );
-
+  token.addGrant(voiceGrant);
   res.json({ token: token.toJwt() });
+});
+
+/* THIS IS THE KEY FIX */
+app.post("/voice", (req, res) => {
+  const twiml = new twilio.twiml.VoiceResponse();
+  const to = req.body.To;
+
+  if (to) {
+    twiml.dial(to);
+  } else {
+    twiml.say("No destination number provided");
+  }
+
+  res.type("text/xml");
+  res.send(twiml.toString());
 });
 
 app.listen(process.env.PORT || 3000);
