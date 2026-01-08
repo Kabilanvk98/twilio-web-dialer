48ee34e5656b0636da094982a7913533cad2ecce
Kabilanvk98 <57007075+Kabilanvk98@users.noreply.github.com>
2026-01-09 01:26:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,14 +8,12 @@ app.use(express.urlencoded({ extended: false }));
 const ACCOUNT_SID = process.env.ACCOUNT_SID;
 const API_KEY = process.env.API_KEY;
 const API_SECRET = process.env.API_SECRET;
+const CALLER_ID = process.env.CALLER_ID;
 
+/* TOKEN FOR BROWSER */
 app.get("/token", (req, res) => {
   const AccessToken = twilio.jwt.AccessToken;
   const VoiceGrant = AccessToken.VoiceGrant;
-
-  const voiceGrant = new VoiceGrant({
-    outgoingApplicationSid: "unused"
-  });
 
   const token = new AccessToken(
     ACCOUNT_SID,
@@ -24,17 +22,17 @@ app.get("/token", (req, res) => {
     { identity: "agent1" }
   );
 
-  token.addGrant(voiceGrant);
+  token.addGrant(new VoiceGrant());
   res.json({ token: token.toJwt() });
 });
 
-/* THIS IS THE KEY FIX */
+/* THIS IS REQUIRED FOR CALLS */
 app.post("/voice", (req, res) => {
   const twiml = new twilio.twiml.VoiceResponse();
   const to = req.body.To;
 
   if (to) {
-    twiml.dial(to);
+    twiml.dial({ callerId: CALLER_ID }, to);
   } else {
     twiml.say("No destination number provided");
   }
